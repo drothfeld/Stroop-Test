78b7a4b3d79a38b89f2f5fd1d61bba7f91ba5b46
Dylan Rothfeld <drothfel@gmail.com>
2019-06-20 12:26:22 -0700

    menu view setup

--- Stroop Test/View Controllers/MenuViewController.swift
+++ Stroop Test/View Controllers/MenuViewController.swift
@@ -14,10 +14,45 @@ import UIKit
 // begin a new test session.
 // ==================================================
 class MenuViewController: UIViewController {
+    // Storyboard IBOutlets
+    @IBOutlet weak var AppVersionNumberLabel: UILabel!
+    @IBOutlet weak var TapToBeginLabel: UILabel!
+    
+    // Controller Values
+    let appVersionString: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Setting version number in UI
+        AppVersionNumberLabel.text = "V-" + appVersionString
+        
+        // Setup tap gesture recognizer
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.tappedBackground(_:)))
+        tap.delegate = self as? UIGestureRecognizerDelegate
+        self.view.addGestureRecognizer(tap)
+        
+        // Begin title label blinking animtion
+        TapToBeginLabel.startBlink()
+    }
+    
+    // Transition to SetupViewController when view is tapped
+    @objc func tappedBackground(_ sender: UITapGestureRecognizer) { performSegue(withIdentifier: "toSetup", sender: self) }
+}
+
+// UILabel extension to enable blinking animation
+extension UILabel {
+    // Start UILabel blinking animation
+    func startBlink() {
+        UIView.animate(withDuration: 1.0,
+                       delay:0.0,
+                       options:[.allowUserInteraction, .curveEaseInOut, .autoreverse, .repeat],
+                       animations: { self.alpha = 0.10 },
+                       completion: nil)
+    }
+    // Stop UILabel blinking animation
+    func stopBlink() {
+        layer.removeAllAnimations()
+        alpha = 1
     }
 }
 
