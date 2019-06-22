27ef74a1d32f7f9c8aed9b310c956631e5c8bb24
Dylan Rothfeld <drothfel@gmail.com>
2019-06-22 10:51:50 -0700

    added text field validation for number of words and word time limit text field inputs

--- Stroop Test/View Controllers/Test Session/SetupViewController.swift
+++ Stroop Test/View Controllers/Test Session/SetupViewController.swift
@@ -13,10 +13,34 @@ import UIKit
 // Controls the view where the user chooses parameter
 // options for a new test session.
 // ==================================================
-class SetupViewController: UIViewController {
+class SetupViewController: UIViewController, UITextFieldDelegate {
+    // Storyboard Outlets
+    @IBOutlet weak var NumbWordsTextField: UITextField!
+    @IBOutlet weak var TimeLimitTextField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    // Validate number range text field
+    func validateTextField(uiTextField: UITextField, valueRange: (Int, Int)) {
+        if (Int(uiTextField.text!) == nil) { uiTextField.text = nil }
+        else {
+            if (Int(uiTextField.text!)! < valueRange.0) { uiTextField.text = String(valueRange.0) }
+            if (Int(uiTextField.text!)! > valueRange.1) { uiTextField.text = String(valueRange.1) }
+        }
+    }
+    
+    // Hide keyboard when users presses the done/return key
+    func textFieldShouldReturn(_ scoreText: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    // Number of words text field value changes
+    @IBAction func numbWordsFieldChanges(_ sender: Any) { validateTextField(uiTextField: NumbWordsTextField, valueRange: (1, 40)) }
+    
+    // Word time limit text field value changes
+    @IBAction func wordTimeLimitFieldChanges(_ sender: Any) { validateTextField(uiTextField: TimeLimitTextField, valueRange: (5, 30)) }
 }
